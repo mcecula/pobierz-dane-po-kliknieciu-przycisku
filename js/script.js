3401650098ca849379c6847f5ef41a364e7b0f99
mcecula <mc23051988@gmail.com>
2023-04-28 17:41:03 +0200

    warsztat

---
+++ js/script.js
@@ -0,0 +1,41 @@
+console.log('Warsztat - Pobierz dane po kliknięciu przycisku');
+
+let btnGetData = document.getElementById('get-data');
+
+
+
+const getData = () => {
+    fetch('https://akademia108.pl/api/ajax/get-post.php')
+        .then(res => res.json())
+        .then(data => {
+
+            let pId = document.createElement('p');
+            let pUserId = document.createElement('p');
+            let pTitle = document.createElement('p');
+            let pBody = document.createElement('p');
+            let hr = document.createElement('hr');
+
+            pId.innerText = `Post ID: ${data.id}`;
+            pUserId.innerText = `User ID: ${data.userId}`;
+            pTitle.innerText = `Title ID: ${data.title}`;
+            pBody.innerText = `Body ID: ${data.body}`;
+
+            document.body.appendChild(pId);
+            document.body.appendChild(pUserId);
+            document.body.appendChild(pTitle);
+            document.body.appendChild(pBody);
+            document.body.appendChild(hr);
+
+
+            //console.log(pId);
+        })
+        .catch(error => {
+            console.error(error);
+        })
+    //console.log('getData()')
+};
+
+
+btnGetData.addEventListener('click', getData);
+
+//console.log(btnGetData)
